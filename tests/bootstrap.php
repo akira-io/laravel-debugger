126c19c56193804ede1b42dd5549b3f3129c89f9
kid <kidiatoliny@akira-io.com>
2025-11-20 17:02:25 +0100

    fix: remove no-cache option from Pest test command in tests.yml

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,18 +2,4 @@
 
 declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Test Bootstrap
-|--------------------------------------------------------------------------
-|
-| This file is loaded before running the test suite. You can use it to
-| configure the test environment, set up test helpers, or bootstrap
-| the testing framework.
-|
-*/
-
-// Load the Composer autoloader
 require __DIR__.'/../vendor/autoload.php';
-
-// Add any test helpers or configurations here
